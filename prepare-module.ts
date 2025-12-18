a9672edbe05a09900da0503fe6992b5304c2198c
Baptiste Pillot <baptiste@pillot.fr>
2025-12-18 10:46:19 +0100

    Fix regex path matching by escaping dots

--- prepare-module.ts
+++ prepare-module.ts
@@ -8,9 +8,9 @@ async function prepareModules()
 {
 
 	const namespace = await getNamespace()
-	const case1     = RegExp(`../node_modules/${namespace}/`, 'g')
-	const case2     = RegExp(`../node_modules/`, 'g')
-	const case3     = RegExp(`from.*'../../`, 'g')
+	const case1     = RegExp(`\\.\\./node_modules/${namespace}/`, 'g')
+	const case2     = RegExp(`\\.\\./node_modules/`, 'g')
+	const case3     = RegExp(`from\\s+'\\.\\./\\.\\./`, 'g')
 
 	async function getNamespace()
 	{
